8710bd43587c3bac7b75b855c73f9ba00b840c74
Stephan <stephan.s.p@outlook.com>
2023-11-06 12:17:40 +0100

    Barchart oefening toegevoegd

--- src/components/TestComponent.svelte
+++ src/components/TestComponent.svelte
@@ -1,7 +1,9 @@
 <script>
     import { onMount } from 'svelte';
+    import * as d3 from 'd3';
 
-     const getPlayerInfo = async () => {
+    onMount(async () => {
+        const getPlayerInfo = async () => {
         const res = await fetch('https://api.wiseoldman.net/v2/players/gloopt');
         const data = await res.json();
         console.log(data);
@@ -9,7 +11,21 @@
 
     getPlayerInfo(); 
 
+    let data = [30, 40, 10, 20, 60];
+
+    d3.select('#barChart')
+        .selectAll('rect')
+        .data(data)
+        .join('rect')
+        .attr('x', (d,i) => (i*60))
+        .attr('width', 50)
+        .attr('y', (d) => (300 - d*5))
+        .attr('height', (d) => (d*5))
+        .text('h');
+    })
 </script>
 
 <h1>Test</h1>
 <p>Hello Hello</p>
+
+<svg id='barChart' width=500 height=500></svg>
